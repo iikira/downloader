18e2f32018f5ba2e0cd732398a4d351e206110e6
konica <2571583272@qq.com>
2018-02-22 16:28:41 +0800

    更新下载, 暂时取消动态分配线程

--- cmd/downloader/main.go
+++ cmd/downloader/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/iikira/BaiduPCS-Go/requester"
 	"github.com/iikira/downloader"
 )
 
 var (
 	parallel int
 	testing  bool
+	ua       string
 )
 
 func init() {
 	flag.IntVar(&parallel, "p", 5, "download max parallel")
 	flag.BoolVar(&testing, "t", false, "test mode")
+	flag.StringVar(&ua, "ua", "", "user-agent")
 	flag.Parse()
 }
 
@@ -23,8 +26,11 @@ func main() {
 		return
 	}
 
+	client := requester.NewHTTPClient()
+	client.UserAgent = ua
 	for k := range flag.Args() {
 		downloader.DoDownload(flag.Arg(k), &downloader.Config{
+			Client:   client,
 			Parallel: parallel,
 			Testing:  testing,
 		})
