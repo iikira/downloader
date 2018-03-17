23b907480f45fd7874b616e88fb36f498c697c6a
apple <2571583272@qq.com>
2018-03-17 11:57:17 +0800

    bugfix for 32-bit devices

--- speeds.go
+++ speeds.go
@@ -1,9 +1,9 @@
 package downloader
 
 import (
-	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 // SpeedsStat 统计下载速度
@@ -11,7 +11,6 @@ type SpeedsStat struct {
 	Readed      int64
 	TimeElapsed time.Duration
 	nowTime     time.Time
-	mu          sync.Mutex
 }
 
 // Start 开始统计速度
@@ -27,10 +26,7 @@ func (sps *SpeedsStat) AddReaded(readed int64) {
 
 // EndAndGetSpeedsPerSecond 结束统计速度, 并返回每秒的速度
 func (sps *SpeedsStat) EndAndGetSpeedsPerSecond() (speeds int64) {
-	sps.mu.Lock()
-	sps.TimeElapsed = time.Since(sps.nowTime)
-	sps.mu.Unlock()
-
+	atomic.StoreInt64(((*int64)(unsafe.Pointer(&sps.TimeElapsed))), (int64)(time.Since(sps.nowTime)))
 	if sps.TimeElapsed == 0 {
 		return 0
 	}
